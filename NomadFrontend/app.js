0eef43dcb97c404d5b19dfede4e95dc907c47a65
Javaria <javariab17@gmail.com>
2021-09-07 13:36:52 -0600

    make code more 'dry'/easy to read

--- NomadFrontend/app.js
+++ NomadFrontend/app.js
@@ -13,20 +13,22 @@ localHostUrl = "http://localhost:3001/users"
 // next step document.select modal elements and dynamically change it based on selected card
 
 
-const coachCard = (name, day, timezone, dayColumn) => {
+const spawnCoachCard = (name, day, timezone, dayColumn) => {
     const coachDiv = document.createElement('div')
     coachDiv.className = "shadow p-3 mb-5 bg-white rounded";
 
     const makeAppointmentBtn = document.createElement('button');
     makeAppointmentBtn.className = "btn btn-primary";
     makeAppointmentBtn.innerHTML = "<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#coachModal'> Book Appointment </button>"
+    
     const nameTag = document.createElement('h7');
-    const timezoneTag = document.createElement('p');
-    const availabilityTag = document.createElement('p');
     nameTag.textContent = `Coach: ${name}`;
     nameTag.className = "font-weight-bold"
-    timezoneTag.textContent = timezone;
+    const timezoneTag = document.createElement('p');
+     timezoneTag.textContent = timezone;
+    const availabilityTag = document.createElement('p');
     availabilityTag.textContent = `Availability: ${day[0]}-${day[1]}`;
+    
     coachDiv.append(nameTag,timezoneTag,availabilityTag, makeAppointmentBtn)
     dayColumn.append(coachDiv)
     console.log('done')
@@ -36,38 +38,36 @@ fetch(localHostUrl)
     .then(response => response.json())
     .then(users => users.forEach(user => {
         const {id, name, coach, timezone, monday, tuesday, wednesday, thursday, friday, saturday, sunday} =  user
-        
-        
-        
+        //while statement didnt work, changed to if statements from "switch/case statement"
         if (coach === true) {
             if(monday[0] != ['N/A'] || monday[1] != undefined ){
                 let dayColumn = document.getElementById(`monday`);
-                coachCard(name, monday, timezone, dayColumn)
+                spawnCoachCard(name, monday, timezone, dayColumn)
             } 
             if (tuesday[0] != ['N/A'] || tuesday[1] != undefined )  {
                 let dayColumn = document.getElementById(`tuesday`);
-                coachCard(name, tuesday, timezone, dayColumn)
+                spawnCoachCard(name, tuesday, timezone, dayColumn)
             } 
             if (wednesday[0] != ['N/A'] || wednesday[1] != undefined)  {
                 let dayColumn = document.getElementById(`wednesday`);
-                coachCard( name, wednesday, timezone, dayColumn)
+                spawnCoachCard( name, wednesday, timezone, dayColumn)
             } 
             if(thursday[0] != ['N/A'] || thursday[1] != undefined)  {
                 console.log(thursday)
                 let dayColumn = document.getElementById(`thursday`);
-                coachCard(name, thursday, timezone, dayColumn)
+                spawnCoachCard(name, thursday, timezone, dayColumn)
             } 
             if (friday[0] != ['N/A'] || friday[1] != undefined)  {
                 let dayColumn = document.getElementById(`friday`);
-                coachCard(name, friday,timezone,dayColumn)
+                spawnCoachCard(name, friday,timezone,dayColumn)
             } 
             if (saturday[0] != ['N/A'] || saturday[1] != undefined)  {
                 let dayColumn = document.getElementById(`saturday`);
-                coachCard(name, saturday, timezone, dayColumn)
+                spawnCoachCard(name, saturday, timezone, dayColumn)
             }
             if (sunday[0] != ['N/A'] || sunday[1] != undefined)  {
                 let dayColumn = document.getElementById(`sunday`);
-                coachCard(name, sunday, timezone, dayColumn)
+                spawnCoachCard(name, sunday, timezone, dayColumn)
             }
             
             }
